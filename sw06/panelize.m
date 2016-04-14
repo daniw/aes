6af5b4bf563f149e7dc31e3f5eb281a75ec820f9
daniw <dani.winz@bluewin.ch>
2016-04-14 07:17:34 +0200

    Function to calculate panelization

---
+++ sw06/panelize.m
@@ -0,0 +1,30 @@
+function [nof_panels, nof_x, nof_y, leftover_x, leftover_y] = panelize(roof_x, roof_y, panel_x, panel_y, beta, alpha, spacing_min)
+% PANELIZE Function to estimate the number of panels placed on a roof
+%   Parameters: 
+%       roof_x:      roof size in x direction
+%       roof_y:      roof size in y direction
+%       panel_x:     panel size in x direction
+%       panel_y:     panel size in y direction
+%       beta:        angle of panels from horizontal mounting in radiant
+%       alpha:       minimum free angle behind panels in radiant
+%       spacing_min: minimum spacing behind panels for mounting and cleaning
+%   Return values:
+%       nof_panels: total number of panels
+%       nof_x:      number of panels in x direction
+%       nof_y:      number of panels in y direction
+%       leftover_x: not used space in x direction
+%       leftover_y: not used space in y direction
+    nof_x = floor((roof_x - spacing_min) ./ panel_x);
+    leftover_x = roof_x - (nof_x .* panel_x);
+    panel_y_corr = panel_y .* cos(beta);
+    panel_h = panel_y .* sin(beta);
+    spacing_angle = panel_h ./ tan(alpha);
+    if (spacing_angle > spacing_min)
+        spacing = spacing_angle;
+    else
+        spacing = spacing_min;
+    end
+    nof_y = floor((roof_y - panel_y) ./ (panel_y_corr + spacing)) + 1;
+    leftover_y = roof_y - ((nof_y - 1) .* (panel_y_corr + spacing) + panel_y_corr);
+    nof_panels = nof_x .* nof_y;
+end
